9f19ee06016b14bdadd75a82943602d0e238c239
Mfawzy9 <mahmoud.fawzy.992000@gmail.com>
2024-09-04 00:09:53 +0300

    accessPage commit

--- src/components/NoAccess/NoAccess.jsx
+++ src/components/NoAccess/NoAccess.jsx
@@ -30,7 +30,7 @@ export default function NoAccess() {
               <div className="flex items-center flex-wrap justify-center gap-3 mt-4">
                 <Link
                   to={"/login"}
-                  className="group mt-2 relative overflow-hidden bg-green-700 focus:ring-4 focus:ring-green-300 inline-flex items-center px-3 sm:px-7 py-2.5 rounded-lg text-white justify-center"
+                  className="group mt-2 relative overflow-hidden bg-blue-700 focus:ring-4 focus:ring-green-300 inline-flex items-center px-3 sm:px-7 py-2.5 rounded-lg text-white justify-center"
                 >
                   <span className="z-40 flex items-center">
                     <VscSignIn className="me-2 text-xl" />
@@ -40,7 +40,7 @@ export default function NoAccess() {
                 </Link>
                 <Link
                   to={"/signup"}
-                  className="group mt-2 relative overflow-hidden bg-blue-700 focus:ring-4 focus:ring-orange-300 inline-flex items-center px-7 py-2.5 rounded-lg text-white justify-center"
+                  className="group mt-2 relative overflow-hidden bg-green-700 focus:ring-4 focus:ring-orange-300 inline-flex items-center px-7 py-2.5 rounded-lg text-white justify-center"
                 >
                   <span className="z-40 flex items-center">
                     <fontAwesome.FaUserPlus className="me-2 text-xl" />
@@ -51,12 +51,12 @@ export default function NoAccess() {
               </div>
               <span className="text-2xl font-bold">OR</span>
               <button
-                onClick={() => navigate(-3)}
+                onClick={() => navigate("/")}
                 className="group mt-2 relative overflow-hidden bg-orange-700 focus:ring-4 focus:ring-green-300 inline-flex items-center px-3 sm:px-7 py-2.5 rounded-lg text-white justify-center"
               >
                 <span className="z-40 flex items-center">
-                  <fontAwesome.FaArrowAltCircleLeft className="me-2 text-xl" />
-                  {"Continue shopping"}
+                  <fontAwesome.FaHome className="me-2 text-xl" />
+                  {"Go To Home Page"}
                 </span>
                 <div className="absolute inset-0 h-[200%] w-[200%] rotate-45 translate-x-[-70%] transition-all group-hover:scale-100 bg-white/30 group-hover:translate-x-[50%] z-20 duration-1000"></div>
               </button>
